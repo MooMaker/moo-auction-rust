873d6b38c0eee27620a348d05b2cc452b62a82e1
6124437 <arjanvanhoogdalem@gmail.com>
2023-05-13 15:37:58 +0200

    post function json body not working

--- src/handlers.rs
+++ src/handlers.rs
@@ -10,5 +10,10 @@ pub async fn ws_handler(ws: warp::ws::Ws, clients: Clients) -> Result<impl Reply
 
 pub async fn auction_handler() -> Result<impl warp::Reply> {
     println!("auction_handler");
+
+    //TODO: fwd to MM via ws
+    // ws::publish_auction(json_input, &clients.clone());
+
+    //TODO
     Ok(StatusCode::NO_CONTENT)
 }
